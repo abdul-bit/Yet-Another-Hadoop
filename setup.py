7d4b7699e3c96a203af7cc651360085fcc218a7c
Advit Gandhi <advitgandhi12@gmail.com>
2021-11-22 10:18:12 +0530

    Implemented namenode and datanode

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import json
 import os
-# from pickle import TRUE
-# import subprocess
+import math
 f = open('config_sample.json')
 data = json.load(f)
 block_size = data["block_size"]
@@ -18,22 +17,37 @@ fs_path = data["fs_path"]
 dfs_setup_config = data["dfs_setup_config"]
 print(block_size, path_to_datanodes, path_to_namenodes, replication_factor, num_datanodes, datanode_size,
       sync_period, datanode_log_path, namenode_log_path, namenode_checkpoints, fs_path, dfs_setup_config)
-os.makedirs(path_to_datanodes)
-os.makedirs(path_to_namenodes)
-os.makedirs(datanode_log_path)
-os.makedirs(namenode_log_path)
-os.makedirs(namenode_checkpoints)
-os.makedirs(fs_path)
-os.makedirs(dfs_setup_config)
+# os.makedirs(path_to_datanodes)
+# os.makedirs(path_to_namenodes)
+# os.makedirs(datanode_log_path)
+# os.makedirs(namenode_log_path)
+# os.makedirs(namenode_checkpoints)
+# os.makedirs(fs_path)
+# os.makedirs(dfs_setup_config)
 f.close()
-
-for i in range(1, num_datanodes):
-    filename = 'DataNode'
-    txt = '.txt'
-    filename1 = path_to_datanodes+filename+str(i)+txt
-    with open(filename1, 'w') as f:
-        f.seek(1024*64)
-        f.write('0')
+size = os.path.getsize('C:\PESU college stuff\Sem 5\Sem 5\Big Data\Project\cleaned_nba_data.csv')
+num_of_files=math.ceil(size/(block_size*1024))
+f_block="file_block"
+dnode="datanode"
+dict = {}
+for i in range(num_datanodes):
+      temp=dnode+str(i)
+      dict[temp]=[]
+print(size,num_of_files)
+for i in range(num_of_files):
+      t=i%num_datanodes
+      temp=dnode+str(t)
+      dict[temp].append(f_block+str(i))
+f = open(path_to_namenodes+"namenode.json", "w")
+json.dump(dict, f)
+f.close()
+# for i in range(1, num_datanodes):
+#     filename = 'DataNode'
+#     txt = '.txt'
+#     filename1 = path_to_datanodes+filename+str(i)+txt
+#     with open(filename1, 'w') as f:
+#         f.seek(1024*64)
+#         f.write('0')
         # exe = 'hachoir-metadata'
         # process = subprocess.Popen(
         #     [exe, filename1], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=TRUE)
