6abb6e6b4abb79eaf9bc0278e57f46982fad7afd
Narendiran Arthanarieswaran <narendiran.work@gmail.com>
2021-12-06 12:00:23 +0530

    Hardcoded MapReduce

--- yah/__main__.py
+++ yah/__main__.py
@@ -1,24 +1,49 @@
-from os import path
-import sys, json
+import sys, json, os, argparse
 # from .classmodule import MyClass
 from .funcmodule import dfs_put
 
+f = open('config_sample.json')
+data = json.load(f)
+block_size = data["block_size"]
+path_to_datanodes = data["path_to_datanodes"]
+path_to_namenodes = data["path_to_namenodes"]
+replication_factor = data["replication_factor"]
+num_datanodes = data["num_datanodes"]
+datanode_size = data["datanode_size"]
+sync_period = data["sync_period"]
+datanode_log_path = data["datanode_log_path"]
+namenode_log_path = data["namenode_log_path"]
+namenode_checkpoints = data["namenode_checkpoints"]
+fs_path = data["fs_path"]
+dfs_setup_config = data["dfs_setup_config"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-mapper')
+parser.add_argument('-reducer')
+parser.add_argument('-input')
+parser.add_argument('-output')
+# pargs = parser.parse_args()
+
 def main():
-    print('in main')
     args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+    if args[0] == 'mkdir':
+        fw = open(path_to_namenodes+'/namenode.json', 'w')
+        if os.stat(path_to_namenodes+'/namenode.json').st_size == 0:
+            dictionary_dir = {}
+            dictionary_dir[args[1]] = None
+            json.dump(dictionary_dir, fw)
+        else:
+            fr = open(path_to_namenodes+'/namenode.json', 'r')
+            namenode = json.load(fr)
+            if args[1] in namenode:
+                print('directory already exists, create a new one with a different name')
 
-    # my_function('hello world')
-
-    # my_object = MyClass('Thomas')
-    # my_object.say_name()
-    f = open('config_sample.json')
-    data = json.load(f)
-    block_size = data["block_size"]
-    path_to_namenodes = data["path_to_namenodes"]
-    num_datanodes = data["num_datanodes"]
-    dfs_put(num_datanodes=num_datanodes,path_to_namenodes=path_to_namenodes,block_size=block_size)
+    elif args[0] == 'put':
+        dfs_put(num_datanodes=num_datanodes,
+                path_to_namenodes=path_to_namenodes, block_size=block_size, directory=args[2], file_location=args[1])
+    else:
+        for i in range(1,4):
+            os.system('cat /Users/naren/Downloads/Datanode/dataset_{i}.txt | Python3 /Users/naren/Downloads/mapper.py >> /Users/naren/Downloads/op.txt'.format(i=i))
+        os.system('cat /Users/naren/Downloads/op.txt | sort -k 1,1 | Python3 /Users/naren/Downloads/reducer.py and')
 if __name__ == '__main__':
     main()
--- yah/funcmodule.py
+++ yah/funcmodule.py
@@ -1,42 +1,27 @@
-import json, os, math
+import json
+import os
+import math
+from fsplit.filesplit import Filesplit
 
-def dfs_put(num_datanodes,path_to_namenodes,block_size):
-    size = os.path.getsize('/Users/naren/Downloads/embedding-sample2.json')
-    num_of_files=math.ceil(size/(block_size*1024))
-    f_block="file_block"
-    dnode="datanode"
-    input_path = "/Users/naren/Downloads/embedding-sample2.json"
-    dict = {}
-    for i in range(1, num_datanodes+1):
-        dict['datanode'+str(i)] = []
+fs = Filesplit()
 
-    CHUNK_SIZE = (block_size-1)*1024
 
-    file_number = 1
-    with open(input_path, encoding="utf8") as f:
-        chunk = f.read(CHUNK_SIZE)
-        while chunk:
-            with open('/Users/naren/Downloads/Datanode/filechunk' + str(file_number), 'w', encoding="utf8") as chunk_file:
-                chunk_file.write(chunk)
-            z = 'datanode'+str(((file_number-1) % num_datanodes)+1)
-            dict[z].append('file_chunk'+str(file_number))
-            file_number += 1
-            chunk = f.read(CHUNK_SIZE)
+def dfs_put(num_datanodes, path_to_namenodes, block_size, directory, file_location):
+    size = os.path.getsize(file_location)
+    fs.split(file=file_location,
+             split_size=block_size*(2**10), output_dir="/Users/naren/Downloads/Datanode", newline=True)
+    f = open(path_to_namenodes+'/namenode.json', 'w+')
+    num_of_files = math.ceil(size/(block_size*1024))
 
-    f_json = open(path_to_namenodes+"name_node.json", 'w')
-    json.dump(dict, f_json)
-    f_json.close()
-    # dict = {}
-    # for i in range(num_datanodes):
-    #     temp=dnode+str(i)
-    #     dict[temp]=[]
-    # print(size,num_of_files)
-    # for i in range(num_of_files):
-    #     t=i%num_datanodes
-    #     temp=dnode+str(t)
-    #     dict[temp].append(f_block+str(i))
-    # f = open(path_to_namenodes+"namenode.json", "w")
-    # print(path_to_namenodes)
-    # json.dump(dict, f)
-    # f.close()
-
+    dict1 = {}
+    for j in range(1, num_of_files+1):
+        temp = os.path.splitext(os.path.basename(file_location))[0]+'_'+str(j)
+        dict1[temp] = []
+    for j in range(1, num_of_files+1):
+        temp = os.path.splitext(os.path.basename(file_location))[0]+'_'+str(j)
+        datanode = 'datanode'+str((j-1) % num_datanodes+1)
+        dict1[temp].append(datanode)
+    dict_file = {}
+    dict_file[os.path.basename(file_location)] = dict1
+    temp_json = {directory: dict_file}
+    json.dump(temp_json, f)
